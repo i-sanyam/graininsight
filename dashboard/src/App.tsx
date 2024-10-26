d2a46789df81542e4aa4edb79c302d8609360a2a
Sanyam Aggarwal <sanyam.nitj@gmail.com>
2024-10-26 13:24:07 +0530

    Grain Insights Logo

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,15 +1,23 @@
 import { SignedIn } from '@clerk/clerk-react';
 import ApiResponseHandler from './components/ApiResponseHandler';
 import Header from './components/Header';
+import grainInsightLogo from './assets/grain-insight-logo.png';
 
 export default function App() {
   return (
     <div>
-      <Header />
-      <h1>Grain Insight Dashboard</h1>
-      <SignedIn>
-        <ApiResponseHandler />
-      </SignedIn>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <h1>Grain Insights Dashboard</h1>
+        <img src={grainInsightLogo} alt="Grain Insights Logo" />
+      </div>
+      <div>
+        <SignedIn>
+          <ApiResponseHandler />
+        </SignedIn>
+      </div>
     </div>
   );
 }
